a509789c66c5b52f4142d4160a19abf2742d426e
Théo Rousseau <48555250+zewcro@users.noreply.github.com>
2022-11-28 14:07:54 +0100

    first migration from model

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django </h1>')
+
+def about(request):
+    return HttpResponse('<h1>à propos</h1> <p> Nous adorons merch! </p>')
