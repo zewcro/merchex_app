51c90d0b4499d6d104626758464c9f9bdd96bca7
Théo Rousseau <48555250+zewcro@users.noreply.github.com>
2022-11-29 14:10:01 +0100

    add new options for admin panel

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from listings.models import Band
-
+from listings.models import Listing
 
 admin.site.register(Band)
+admin.site.register(Listing)
--- listings/models.py
+++ listings/models.py
@@ -10,8 +10,14 @@ class Band(models.Model):
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     biography = models.fields.CharField(max_length=1000)
-    year_formed = models.fields.IntegerField(
-    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
-    )
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
+
+
+class Listing(models.Model):
+    title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
