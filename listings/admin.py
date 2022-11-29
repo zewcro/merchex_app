5f35b5ce92f948acd2a242470e4eae3c88bcb673
Théo Rousseau <48555250+zewcro@users.noreply.github.com>
2022-11-29 15:17:58 +0100

    admin edit

--- listings/admin.py
+++ listings/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
 from listings.models import Band
+
 from listings.models import Listing
 
-admin.site.register(Band)
-admin.site.register(Listing)
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name','year_formed', 'genre', 'active')
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title','sold')
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing,ListingAdmin)
