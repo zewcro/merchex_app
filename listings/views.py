4de250cd289869fa3cd5b1337aa95caee19720a9
zewcro <theorousseau75@gmail.com>
2022-11-30 00:29:39 +0100

    Lisez des données dans une vue en liste et une vue détaillée

--- listings/views.py
+++ listings/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from listings.models import Band
 
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 'listings/hello.html',{'bands': bands})
+    return render(request, 'listings/band_list.html',{'bands': bands})
+
+def band_detail(request,id):
+    # we ask to return the 'band' object with the submited id from url  
+    band = Band.objects.get(id=id)
+    return render(request, 'listings/band_detail.html', {'band':band} ) # passage de l'id au model
--- merchex/urls.py
+++ merchex/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from listings import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', views.hello)
+        path('admin/', admin.site.urls),
+        path('bands/', views.band_list, name='band-list'),
+        path('bands/<int:id>/', views.band_detail, name='band-detail'),
 ]
